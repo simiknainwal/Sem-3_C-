1b31b578985a64c1711e81a949e419e25c173b33
Simik Nainwal <simikjio@gmail.com>
2025-08-30 08:39:20 +0530

    Inheritance

---
+++ C++ programs/inheritance.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Test{
+    public:
+    int a;
+    Test(){
+        a=0;
+    }
+    Test(int a){
+        this->a=a;
+    }
+};
+
+class Best:public Test{
+public:
+    Best(){
+       a=0;
+    }
+    Best(int a){
+        this->a=a;
+    }
+void display(){
+    // a=20;
+    cout<<a;
+}
+};
+
+int main(){
+    Best ob1(999);
+    ob1.display();
+}
